898a938daf8924917ef88066265a598a0a6378a3
Funya1337 <funya7654@gmail.com>
2024-10-28 00:44:57 +0300

    added: colision detection, floor color

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 #include "engine.h"
 #include "math.h"
@@ -23,25 +24,64 @@ void printMap(char map[nMapWidth][nMapHeight]);
 int main() {
     const char* map =
         "#################..............##..............##..............##..............##..............##..."
-        "...........##..............##..............##..............##..............##..............##......."
+        "...........##......#.......##..............##..............##..............##..............##......."
         ".......##..............##..............#################";
     // char map[nMapWidth][nMapHeight];
     // fillMapFromFile(map);
     initscr();
+    start_color();
+    noecho();
+    cbreak();
+    curs_set(0);
+    nodelay(stdscr, TRUE);
 
-    // for (int i = 0; i < nScreenHeight; ++i) {
-    //     for (int j = 0; j < nScreenWidth; ++j) {
-    //         mvaddch(i, j, '.');
-    //     }
-    // }
+    init_color(COLOR_CYAN, 201, 195, 195);  // Custom Grey (R, G, B)
+    init_pair(1, COLOR_WHITE, COLOR_BLACK);
+    init_pair(2, COLOR_CYAN, COLOR_BLACK);
 
-    // printMap(map);
+    struct timespec t1, t2;
+    clock_gettime(CLOCK_MONOTONIC, &t1);
 
     while (isRunning()) {
+        clock_gettime(CLOCK_MONOTONIC, &t2);
+        double fElapsedTime = (t2.tv_sec - t1.tv_sec) + (t2.tv_nsec - t1.tv_nsec) / 1e9;
+        t1 = t2;
+
+        switch (getch()) {
+            case 'q':
+                setIsRunning();
+                break;
+            case 68:
+                fPlayerA -= 200 * fElapsedTime;
+                break;
+            case 67:
+                fPlayerA += 200 * fElapsedTime;
+                break;
+            case 'w':
+                fPlayerX += sinf(fPlayerA) * 800 * fElapsedTime;
+                fPlayerY += cosf(fPlayerA) * 800 * fElapsedTime;
+
+                if (map[(int)fPlayerY * nMapWidth + (int)fPlayerX] == '#') {
+                    fPlayerX -= sinf(fPlayerA) * 800 * fElapsedTime;
+                    fPlayerY -= cosf(fPlayerA) * 800 * fElapsedTime;
+                }
+                break;
+            case 's':
+                fPlayerX -= sinf(fPlayerA) * 800 * fElapsedTime;
+                fPlayerY -= cosf(fPlayerA) * 800 * fElapsedTime;
+
+                if (map[(int)fPlayerY * nMapWidth + (int)fPlayerX] == '#') {
+                    fPlayerX += sinf(fPlayerA) * 800 * fElapsedTime;
+                    fPlayerY += cosf(fPlayerA) * 800 * fElapsedTime;
+                }
+                break;
+        }
+
         for (int x = 0; x < nScreenWidth; ++x) {
             float fRayAngle = (fPlayerA - fFOV / 2.0) + ((float)x / (float)nScreenWidth) * fFOV;
             float fDistanceToWall = 0;
             int bHitWall = 0;
+            // int bBoundary = 0;
 
             float fEyeX = sinf(fRayAngle);
             float fEyeY = cosf(fRayAngle);
@@ -65,41 +105,51 @@ int main() {
             int nFloor = nScreenHeight - nCeiling;
 
             int nShade = ' ';
+            int nShadeFloor = ' ';
+
+            // NOT WORKING WITH BLOCK ASCII CHARACTERS (TO DO)
+            // if (fDistanceToWall <= fDepth / 4.0)
+            //     nShade = 0x2588;
+            // else if (fDistanceToWall < fDepth / 3.0)
+            //     nShade = 0x2593;
+            // else if (fDistanceToWall < fDepth / 2.0)
+            //     nShade = 0x2592;
+            // else if (fDistanceToWall < fDepth) {
+            //     nShade = 0x2591;
+            // } else
+            //     nShade = ' ';
 
             if (fDistanceToWall <= fDepth / 4.0)
-                nShade = 0x2588;
+                nShade = '#';
             else if (fDistanceToWall < fDepth / 3.0)
-                nShade = 0x2593;
+                nShade = '+';
             else if (fDistanceToWall < fDepth / 2.0)
-                nShade = 0x2592;
-            else if (fDistanceToWall < fDepth) {
-                init_pair(1, COLOR_BLACK, COLOR_RED);
-                nShade = 0x2591;
-            } else
-                nShade = ' ';
+                nShade = '-';
+            else
+                nShade = '.';
 
             for (int y = 0; y < nScreenHeight; ++y) {
                 if (y < nCeiling)
                     mvaddch(y, x, ' ');
-                else if (y > nCeiling && y <= nFloor)
-                    mvaddch(y, x, (char)nShade);
-                else
-                    mvaddch(y, x, ' ');
+                else if (y > nCeiling && y <= nFloor) {
+                    mvaddch(y, x, (char)nShade | COLOR_PAIR(1));
+                } else {
+                    float b = 1.0 - (((float)y - nScreenHeight / 2.0) / ((float)nScreenHeight / 2.0));
+                    if (b < 0.25)
+                        nShadeFloor = '#';
+                    else if (b < 0.5)
+                        nShadeFloor = '+';
+                    else if (b < 0.75)
+                        nShadeFloor = '-';
+                    else if (b < 0.9)
+                        nShadeFloor = ':';
+                    else
+                        nShadeFloor = '.';
+                    mvaddch(y, x, nShadeFloor | COLOR_PAIR(2));
+                }
             }
         }
         refresh();
-        char ch = getch();
-        if (ch == 'q') setIsRunning();
-        if (ch == 'a') fPlayerA -= (0.3);
-        if (ch == 'd') fPlayerA += (0.3);
-        if (ch == 'w') {
-            fPlayerX += sinf(fPlayerA) * 1.0;
-            fPlayerY += cosf(fPlayerA) * 1.0;
-        }
-        if (ch == 's') {
-            fPlayerX -= sinf(fPlayerA) * 1.0;
-            fPlayerY -= cosf(fPlayerA) * 1.0;
-        }
     }
     endwin();
     return 0;
